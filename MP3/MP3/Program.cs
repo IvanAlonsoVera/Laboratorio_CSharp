9b3cd1a070aa19a32febfc561e585eb4f359ea8e
Iván Alonso Vera <alonso.vera.ivan@gmail.com>
2022-12-14 18:03:00 +0100

    141222

---
+++ MP3/MP3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3
+{
+    internal class Program
+    {
+        struct tipoFicha
+        {
+            public string nombreFich;
+            public long tamaño;
+        }
+        static void Main(string[] args)
+        {
+            tipoFicha[] fichas = new tipoFicha[1000];
+            int numeroFichas = 0;
+            int opcion;
+            int tamañoBusqueda;
+            string ficheroBusqueda;
+            do
+            {
+                Console.WriteLine("elije opcion: ");
+                Console.WriteLine("1-Añadir datos");
+                Console.WriteLine("2-Mostrar nombres ficheros");
+                Console.WriteLine("3-Ficheros mayores de un determinado tamaño");
+                Console.WriteLine("4-Ver datos de un fichero");
+                Console.WriteLine("5-Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        if(numeroFichas < 1000)
+                        {
+                            Console.Write("Nombre del Fichero?: ");
+                            fichas[numeroFichas].nombreFich = Console.ReadLine();
+                            Console.Write("Tamaño del Fichero?: ");
+                            fichas[numeroFichas].tamaño = Convert.ToInt64(Console.ReadLine());
+                            numeroFichas++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Completo");
+                        }
+                        break;
+                    case 2:
+                        foreach(var ficha in fichas)
+                        {
+                            if (ficha.nombreFich != null)
+                            {
+                                Console.WriteLine(ficha.nombreFich);
+                            }
+                            else
+                            {
+                                
+                            }
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("A partir de que tamaño?: ");
+                        tamañoBusqueda = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < numeroFichas; i++)
+                        {
+                            if (fichas[i].tamaño >= tamañoBusqueda)
+                            {
+                                Console.WriteLine("Nombre: {0} - Tamaño {1}", fichas[i].nombreFich, fichas[i].tamaño);
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Que fichero quieres? ");
+                        ficheroBusqueda= Console.ReadLine();
+                        for (int i = 0; i < numeroFichas; i++)
+                        {
+                            if (fichas[i].nombreFich == ficheroBusqueda)
+                            {
+                                Console.WriteLine("Nombre: {0} - Tamaño {1}", fichas[i].nombreFich, fichas[i].tamaño);
+                            }
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("fin");
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcion != 5);
+
+            Console.ReadLine();
+        }
+    }
+}
--- PrimeraApp/PrimeraApp/Program.cs
+++ PrimeraApp/PrimeraApp/Program.cs
@@ -10,6 +10,121 @@ namespace PrimeraApp
     {
         static void Main(string[] args)
         {
+            //int
+            //int primerNumero;
+            //int segundoNumero;
+            //int suma;
+
+            //Console.Write("introduce el primer numero:");
+            //primerNumero = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("introduce el segundo numero:");
+            //segundoNumero = Convert.ToInt32(Console.ReadLine());
+            //suma = primerNumero + segundoNumero;
+            //Console.WriteLine("la suma de {0} + {1} es igual a: {2}",primerNumero, segundoNumero, suma);
+            //Console.Read();
+
+            //(a,b) por teclado
+            // -----------------------------------------------el programa comprueba (a+b)*(a-b)=a2-b2
+
+            //int B;
+            //int A;
+            //int ope1;
+            //int ope2;
+            //Console.Write("introduce el numero A: ");
+            //B = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("introduce el numero B: ");
+            //A = Convert.ToInt32(Console.ReadLine());
+            //ope1 = (A + B) * (A - B);
+            //ope2 = (A * A) - (B * B);
+            //Console.WriteLine("A = {0} , B = {1} ; (a+b)*(a-b)={2} ; a2-b2={3}", A, B, ope1,ope2);
+            //Console.Read();
+
+            //recoje Cº --> Kelvin, fh
+
+            //int C;
+            //int K;
+            //int F;
+            //Console.Write("Introcude la temp en Cº: ");
+            //C = Convert.ToInt32(Console.ReadLine());
+            //K = C + 273;
+            //F = ((C * 18) / 10) + 32;
+            //Console.WriteLine("La Temperatura es de : {0}C, {1}K, {2}F", C, K, F);
+            //Console.Read();
+
+            // ----------------------------------------------Estructuras de control
+
+            //int numero;
+            //Console.Write("Introcude numero: ");
+            //numero = Convert.ToInt32(Console.ReadLine());
+
+            //if(numero > 0)
+            //{
+            //    Console.WriteLine("el numero es positivo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("es negativo");
+            //}
+            //Console.Read();
+
+            // ---------------------------------------------------Operadores logicos
+
+            //int opcion, usuario;
+            //opcion = 1;
+            //usuario = 2;
+
+            //if ((opcion == 1) && (usuario == 2))
+            //{
+            //    Console.WriteLine("eso es asi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("alguna no se cumple");
+            //}
+
+            //// ternario
+
+            ////numeromayor = a<b ? a : b;
+
+            //int a, b, ope, res;
+            //Console.Write("escribe un numero: ");
+            //a=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("escribe otro numero: ");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("escribe una operacion (1=res; otro=suma): ");
+            //ope = Convert.ToInt32(Console.ReadLine());
+            //res = (ope == 1)? a-b : a + b;
+            //Console.Write("el resultado es: {0}",res);
+
+            //string frase = "Hola";
+            //Console.WriteLine(frase);
+
+            //if (frase == "Hola")
+            //{
+            //    Console.WriteLine("es igual");
+            //}
+
+            //string nombre;
+            //Console.Write("Tu nombre please: ");
+            //nombre = Console.ReadLine();
+
+            //switch (nombre)
+            //{
+            //    case "juan": Console.WriteLine("hola juan");
+            //        break;
+            //    case "javier":
+            //        Console.WriteLine("hola javi");
+            //        break;
+            //    case "ivan":
+            //        Console.WriteLine("hola ivan");
+            //        break;
+            //    default:
+            //        Console.WriteLine("hola desconocido");
+            //        break ;
+            //}
+
+
+            Console.Read();
         }
     }
 }
